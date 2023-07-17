7636fc71a437ad7db2d1898c9e14ee9168b389b6
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-07-17 23:18:10 +0300

    created markup and realized logic

---
+++ index.js
@@ -0,0 +1,30 @@
+const refs = {
+  form: document.querySelector("form"),
+  resultRow: document.querySelector("p"),
+};
+
+const findUniqueSymbol = (word) => {	
+	const uniqueSymbols = [...word].filter(symbol => word.indexOf(symbol) === word.lastIndexOf(symbol));	
+	return uniqueSymbols[0];
+};
+
+const textAnalysis = (text) => {
+	const symbols = text.split(" ").map((word) => {		
+    if (word.includes("\n")) {
+		const words = word.split("\n").map((word) => {			
+        return findUniqueSymbol(word);
+      });
+	} else {		
+      return findUniqueSymbol(word);
+    }
+	});	
+	return findUniqueSymbol(symbols.join(","));
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const text = e.currentTarget.elements[0].value;
+  refs.resultRow.textContent = textAnalysis(text);
+};
+
+refs.form.addEventListener("submit", handleSubmit);
